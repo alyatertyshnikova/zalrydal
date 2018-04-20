8afff3058061a26444650e1625e355f556b6fd1c
Alina <macintosh@MacBook-Pro-Macintosh.local>
2018-04-20 20:54:02 +0300

    Registration

---
+++ db_commands.sql
@@ -0,0 +1,12 @@
+/**
+ * Author:  macintosh
+ * Created: 20.04.2018
+ */
+
+CREATE_TABLE users( id integer PRIMARY_KEY AUTO_INCREMENT,
+name VARCHAR(30),
+password VARCHAR(30),
+email VARCHAR(50)
+);
+
+

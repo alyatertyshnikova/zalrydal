cca8b6b70d8b5354032e5c802a0dcc50bdb755d7
Alina <macintosh@MacBook-Pro-Macintosh.local>
2018-05-12 01:05:02 +0300

    Sdelyal

---
+++ files.php
@@ -0,0 +1,15 @@
+<?php
+    echo "h";
+    $dir='d';
+    /*$d = $_POST['path'];
+    echo $d;
+    $dir = opendir($_GET[path]);
+    echo 1;
+    $size = 0;
+    while ($file = readdir($dir)) {
+        $size++;
+        echo $size;
+    }
+}*/
+?>
+

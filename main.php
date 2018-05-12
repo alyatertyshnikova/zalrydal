4eb849f423097874cb3f278ae5f1cae0dac90551
Alina <macintosh@MacBook-Pro-Macintosh.local>
2018-05-12 16:59:18 +0300

    Hash, cookie

--- main.php
+++ main.php
@@ -9,52 +9,40 @@ and open the template in the editor.
         <title>Music map</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+
         <style>
-        .block1{
-            width:1357px;
-            height:628px;
-            margin: auto;
-            position:relative;
-        }
-        .Russia{
-            position:absolute;
-            left:710px;
-            top:145px;
-        }
-    </style>
+            .block1{
+                width:1357px;
+                height:628px;
+                margin: auto;
+                position:relative;
+            }
+            .Russia{
+                position:absolute;
+                left:710px;
+                top:145px;
+            }
+        </style>
     </head>
     <body style="background-image:url('images/waves.gif')">
         <div class="block1">
             <img src='images/worldmap.png'>
-            <div class="Russia">
-                <input type="image" id="Russia" src='images/krestik.png' onclick='playMusic(this)'>
-            </div>
-            <input type="range" id="years" min="1990" max="2010" step="10">
-            <p id="one"></p>
-             
-            <script>
-                var ext = ".mp3";
-                var year = document.getElementById("years");
-                var audio;
-                function playMusic(arg) {
-                    var country = arg.id;
-                    var path = "audio/" + country + "/";
-                    var size = "<?php $dir=$_REQUEST['path'];
-                    echo $dir;?>";
-                    document.write(size);
-                    if (audio != null) {
-                        audio.pause();
-                    }
-                    audio = document.getElementById(year.value);
-                    audio.play();
-                }
-            </script>
-             <!--  $.ajax(type:post, url:'files.php', data:{path:path},
-                       success:function(data){
-                           document.write(data);
-                       } 
-                    )-->
         </div>
+        <div class="Russia">
+            <input type="image" id="Russia" src='images/krestik.png' onclick='playMusic(this)'>
+        </div>
+        <input type="range" id="years" min="1990" max="2010" step="10">
+        <form action="logout.php" method="post"> 
+            <input type="submit" name="exit" value="Log out">
+        </form>
+        <script>
+            var ext = ".mp3";
+            var year = document.getElementById("years");
+            var audio;
+            function playMusic(arg) {
+                var country = arg.id;
+                var path = "audio/" + country + "/";
+            }
+        </script>
     </body>
 </html>

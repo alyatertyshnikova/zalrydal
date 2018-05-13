558a31eb0635cdf89bc315a04cda7d90d04a7ab8
Irina Rekhviashvili <lu.snowflake@gmail.com>
2018-05-13 17:11:46 +0300

    notki

--- count_files.php
+++ count_files.php
@@ -1,21 +1,22 @@
 
 
 
-<?php 
+<?php
 
 function countFolder($dir) {
- return (count(scandir($dir)) - 2);
+    return (count(scandir($dir)) - 2);
 }
 
-if (isset($_POST['path']))
-{
-    $dir=$_POST['path'];
-    $count = countFolder($dir);
-    echo $count;
+if (isset($_POST['path'])) {
+    $dir = $_POST['path'];
+    if (!is_dir($dir)) {
+        $count = 0;
+    } else {
+        $count = countFolder($dir);
+    }
+     echo $count;
+     
+} else {
+    echo "otstoi";
 }
- else {
-    echo "otstoi";     
- }
-
-
 ?>
--- main.php
+++ main.php
@@ -22,6 +22,11 @@ and open the template in the editor.
                 left:710px;
                 top:145px;
             }
+            .Britain{
+                position:absolute;
+                left:544px;
+                top:112px;
+            }
         </style>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
@@ -29,50 +34,77 @@ and open the template in the editor.
     <body style="background-image:url('images/waves.jpg')">
         <div class="block1">
             <img src='images/worldmap.png'>
+            <div class="Russia">
+                <input type="image" id="Russia" name='country' src='images/krestik.png' onclick='playMusic(this)'>
+            </div>
+            <div class="Britain">
+                <input type="image" id="Britain" name='country' src='images/krestik.png' onclick='playMusic(this)'>
+            </div>
         </div>
-        <div class="Russia">
-            <input type="image" id="Russia" src='images/krestik.png' onclick='playMusic(this)'>
-        </div>
-        <input type="range" id="years" min="1990" max="2010" step="10">
+
+        <input type="range" id="years" min="1990" max="2010" step="10" onchange='buttons()'>
         <form action="logout.php" method="post"> 
             <input type="submit" name="exit" value="Log out">
         </form>
+
         <script>
-                var ext = ".mp3";
-                var year = document.getElementById("years");
-                var audio;
+            var ext = ".mp3";
+            var year = document.getElementById("years");
+            var audio;
 
-                function getRandomInt(min, max) {
-                    return Math.floor(Math.random() * (max - min + 1)) + min;
-                }
-
-                function playMusic(arg) {
-                    var country = arg.id;
-                    var path = "audio/" + year.value + "/" + country + "/";
-                    $.ajax({
-                        type: "POST",
-                        url: "count_files.php",
-                        data: {path: path},
-                        success: function (count) {
-                            //alert(count);
-                            //document.write(count);
-                            if (count != 0)
-                            {
-                                var rand = getRandomInt(1, count);
-                                if (audio != null) {
-                                    audio.pause();
-                                }
-                                audio = new Audio(path + rand + ext);
-
-                                audio.play();
-                            } else
-                            {
-                                alert("Sorry, there was no music in this country this year :(");
-                            }
+            function check_button(element) {
+                var path = "audio/" + year.value + "/" + element.id + "/";
+                $.ajax({
+                    type: "POST",
+                    url: "count_files.php",
+                    data: {path: path},
+                    success: function (count) {
+                        if (count == 0){
+                            element.style.visibility = "hidden";
+                        } else {
+                            element.style.visibility = "visible";
                         }
-                    });
+                    }
+                })
+            }
 
-                }
-            </script>
+            function buttons() {
+                var countries = document.getElementsByName("country");
+                var countries_array = Array.prototype.slice.call(countries);
+                countries_array.forEach(check_button);
+                //var max = countries.length;
+                //alert(max);
+
+            }
+            function getRandomInt(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+            function playMusic(arg) {
+                var country = arg.id;
+                var path = "audio/" + year.value + "/" + country + "/";
+                $.ajax({
+                    type: "POST",
+                    url: "count_files.php",
+                    data: {path: path},
+                    success: function (count) {
+                        if (count != 0)
+                        {
+                            var rand = getRandomInt(1, count);
+                            if (audio != null) {
+                                audio.pause();
+                            }
+                            audio = new Audio(path + rand + ext);
+
+                            audio.play();
+                        } else
+                        {
+                            alert("Sorry, there was no music in this country this year :(");
+                        }
+                    }
+                });
+
+            }
+        </script>
     </body>
 </html>
